58ac78106730a13f9ec21a1864d00e4ff528c7f8
rockey1006 <ruoqiwei@my.bridgeport.edu>
2018-07-05 00:25:04 -0400

    Updated queue operations

--- src/queue.java
+++ src/queue.java
@@ -1,8 +1,7 @@
-
 public class queue {
 Node first;
 Node last;
-public class Node{
+public class Node{//内部类
 	int data;
 	Node next;
 	Node(int data){
@@ -10,31 +9,55 @@ public class Node{
 	}
 }
 
-public void enqueue(int data) {
+public void enqueue(int data) {//入队列
 	if(first==null) {
 		first=new Node(data);
 		last=first;
 	}
 		else {
-			last.next=new Node(data);
-			last=last.next;
+			last.next=new Node(data);//谁进来谁在last的后面
+			last=last.next;//然后last就指向谁
 			
 	}
 }
-public int dequeue() {
-	int x=first.data;
-	first=first.next;
+public int dequeue() {//出队列
+	int x=first.data;//返回first的data
+	first=first.next;//first指针更新
 	return x;
+	
+}
+public int Front() {
+	return first.data;//返回队列的第一个数
+}
+public int Size() {
+	int length=1;//返回队列的长度
+	while(first.next!=null) {
+		first=first.next;
+		length++;
+	}
+	return length;
+	
+}
+public boolean isEmpty() {//判断队列是否为空
+	if (first==null)
+		return true;
+	else
+		return false;
 }
 	public static void main(String[] args) {
        queue a=new queue();
-       for (int i=0;i<4;i++) {
+      /* for (int i=0;i<4;i++) {
    		a.enqueue(i);
-   	}
+   	}*/
+      	//System.out.println(a.Size());
+      	//System.out.println(a.Front());
+      	System.out.println(a.isEmpty());
+
+
+   /*System.out.println(a.dequeue());
    	System.out.println(a.dequeue());
    	System.out.println(a.dequeue());
-   	System.out.println(a.dequeue());
-   	System.out.println(a.dequeue());
+   	System.out.println(a.dequeue());*/
 
 
        
