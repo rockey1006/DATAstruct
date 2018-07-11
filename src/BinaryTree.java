fcb00db066af096e13787f0cd27c5bfe9eeb2090
rockey1006 <ruoqiwei@my.bridgeport.edu>
2018-07-11 01:10:01 -0400

    insert updated

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -10,10 +10,12 @@ public BinaryTree(int data,BinaryTree left,BinaryTree right){
 	this.right=right;
 }
 public BinaryTree(int data){
-	this.data=data;//定义完全二叉树的叶结点的构造方法和初始化值
+	this.data=data;//定义叶结点的构造方法和初始化值
 	this.left=null;
 	this.right=null;
 }
+
+
 public boolean isleaf() {//判断是否为叶结点
 	if(this.left==null&&this.right==null) {
 		return true;}
@@ -21,12 +23,11 @@ public boolean isleaf() {//判断是否为叶结点
 			return false;
 	}
 }
-@SuppressWarnings("unused")
-private BinaryTree insert(int data,BinaryTree node) {
-	if(node==null) 
+public BinaryTree insert(int data,BinaryTree node) {
+	if(node==null) {
 		return new BinaryTree(data,null,null);//递归终止的条件
-	
-    if(data<node.data) {//如果要插入的值小于根节点的值
+	}
+	else if(data<node.data) {//如果要插入的值小于根节点的值
     	node.left=insert(data,node.left);////就以根节点的
     } 
     else if(data>node.data) {
@@ -37,15 +38,27 @@ private BinaryTree insert(int data,BinaryTree node) {
 	
 	}
 
-	
-
-	
-
-
-
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BinaryTree a=new BinaryTree(7);
+		BinaryTree b=new BinaryTree(2);
+		BinaryTree c=new BinaryTree(9);
+		BinaryTree d=new BinaryTree(1);
+		BinaryTree e=new BinaryTree(5);
+		BinaryTree f=new BinaryTree(8);
+        a.left=b;
+        a.right=c;
+        b.left=d;
+        b.right=e;
+        c.left=f;
+		//System.out.println(d.isleaf());
+
+        a.insert(12,a);
+		System.out.println(c.right.data);
+
+
+
+
+
 	}
 	
 }
