993b1e7a951d079f3e53819d11aa4c26ded3e172
rockey1006 <ruoqiwei@my.bridgeport.edu>
2018-07-21 02:19:24 -0400

    BinaryHeap

---
+++ src/BinaryHeap.java
@@ -0,0 +1,17 @@
+
+public class BinaryHeap {
+	
+	int size=0;//用于找到堆最后一个元素
+	int[]array;//数组
+	int DEFAULT_SIZE=100;//数组大小
+	public BinaryHeap() {
+		array=new int[DEFAULT_SIZE];//初始化数组，格式：数组名=new int[数组大小];
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
